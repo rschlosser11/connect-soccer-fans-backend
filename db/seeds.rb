e107fe723d965f75a2019259ffca5c05429a345c
rschlosser11 <rosalinschlosser@gmail.com>
2020-08-28 14:45:06 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:'soccerfan1', email:'soccer@me.com', password:'test');
+User.create(username:'tim23', email: 'tim@me.com', password:'test')
+User.create(username:'123sally', email: 'sally@me.com', password:'test')
+User.create(username:'noah43', email: 'noah@me.com', password:'test')
+User.create(username:'susan001', email: 'susan@me.com', password:'test')
+User.create(username:'1jon4443', email: 'jon@me.com', password:'test')
