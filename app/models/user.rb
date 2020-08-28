4eb956ef09ccd8887f98ff5728743cc1523da250
rschlosser11 <rosalinschlosser@gmail.com>
2020-08-28 14:13:58 -0500

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :email, format: {with: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/}, uniqueness: {case_sensitive: false}, presence: true;
+    validates :username, presence: true;
 end
