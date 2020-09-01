62894b5dbe6d39f23e54e3c71280dde05171a46c
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-01 17:19:10 -0500

    add nested route /teams/:team_id/fixtures

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -1,6 +1,10 @@
 class FixturesController < ApplicationController 
     def index
-        fixtures = Fixture.all 
+        if params[:team_id]
+            fixtures = Fixture.find_fixtures_by_team(params[:team_id])
+        else
+            fixtures = Fixture.all 
+        end
         render json: fixtures, except: [:created_at, :updated_at]
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :teams, only: [:index, :show]
-  
+  resources :teams, only: [:index, :show] do 
+    resources :fixtures, only: [:index]
+  end
+
   resources :fixtures, only: [:index, :show]
 end
