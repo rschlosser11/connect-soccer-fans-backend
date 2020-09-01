efa93df290abb52221cf7dc70e0512ed59344e60
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-01 17:12:51 -0500

    add method to Fixture model to find all fixtures by team

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -2,4 +2,8 @@ class Fixture < ApplicationRecord
     belongs_to :home_team, class_name: 'Team'
     belongs_to :away_team, class_name: 'Team'
     scope :upcoming, -> { where('event_date > ?', Time.now).order('event_date DESC') }
+
+    def self.find_fixtures_by_team(team_id)
+        self.where(away_team_id: team_id).or(self.where(home_team_id: team_id))
+    end
 end
