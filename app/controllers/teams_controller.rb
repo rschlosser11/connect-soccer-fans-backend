38434ba805a8851c05bd80ec4c88720502ac794b
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-01 21:21:57 -0500

    add scope for alphabetical teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController 
     def index
-        teams = Team.all 
+        teams = Team.alphabetical 
         render json: teams, except: [:created_at, :updated_at]
     end
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,6 @@ class Team < ApplicationRecord
     has_many :away_fixtures, class_name: 'Fixture', foreign_key: :away_team_id
     has_many :home_fixtures, class_name: 'Fixture', foreign_key: :home_team_id
     validates :team_id, uniqueness: true 
+
+    scope :alphabetical, -> { order('name') }
 end
