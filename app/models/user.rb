34cbc6f1d6336bfcbbea8c9ac8fc94726dfddbb9
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-03 14:43:49 -0500

    add comments table, model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :comments
     validates :email, format: {with: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/}, uniqueness: {case_sensitive: false}, presence: true;
     validates :username, presence: true;
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_220312) do
+ActiveRecord::Schema.define(version: 2020_09_03_193853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.integer "user_id"
+    t.integer "fixture_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "fixtures", force: :cascade do |t|
     t.integer "fixture_id"
